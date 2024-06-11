ccb2ab050ffae51b3764a6ddf29c0cc813378591
Robinson <75579375+iamrbn@users.noreply.github.com>
2024-06-11 11:44:39 +0200

    module version 1.0

--- telekomModule.js
+++ telekomModule.js
@@ -3,8 +3,21 @@
 //=============== Version 1.0 ===============//
 
 
+module.exports.getProgressColor = (fresh, percentage) => {
+	var progressFillColor = '#EC7CB6'
+	if (fresh){
+      if (percentage <= 24) progressFillColor = '#23967F'//green
+      else if (percentage <= 49) progressFillColor = '#FFD60A'//yellow
+      else if (percentage <=74) progressFillColor = '#FF9F0A'//orange
+      else if (percentage >= 75) progressFillColor = '#FF453A'//red
+      else if (percentage >= 85) progressFillColor = '#BF5AF2'//purple
+    } 
+    return progressFillColor
+};
+
+
 module.exports.getFromAPI = async (fm, df, jsonPath) => {
-let data;
+	let data;
 	 try {
 	 	req = new Request("https://pass.telekom.de/api/service/generic/v1/status");
 	 	req.headers = {"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"};
@@ -12,41 +25,45 @@ let data;
 	 	console.warn("\nfetch datas from API")
 	 	//console.log(JSON.stringify(data, null, 2))
 	 	fm.writeString(jsonPath, JSON.stringify(data, null, 2))
-	 	fresh = truer
+	 	fresh = true
 	 	sfSymbol = 'antenna.radiowaves.left.and.right'
 	 	fontColor = Color.dynamic(new Color('#ea0a8e'), new Color('#ffffff'))
 	} catch (error){
-		console.warn("\nfetch datas from iCloud")
+		console.warn(error.message + "\nfetch datas from iCloud")
 		fresh = false
 		sfSymbol = 'wifi.exclamationmark'
 		fontColor = Color.dynamic(new Color('#EC7CB6'), new Color('#EC7CB6'))
 		if (!fm.isFileDownloaded(jsonPath)) await fm.downloadFileFromiCloud(jsonPath)
 		data = JSON.parse(fm.readString(jsonPath), null)
 		//console.log(JSON.stringify(data, null, 2))
-		//console.warn({error})
   }
-  
 unlimited = true
-passName = (data.passName == "Ihr Telekom Datentarif") ? "DataPlan" : data.passName
+passName = (data.passName.includes(" Telekom ")) ? "DataPlan" : data.passName
 
 if (!passName.includes('unlimited')){
     unlimited = false
     usedPercentage = data.usedPercentage
     remainingTimeStr = data.remainingTimeStr
-    usedVolumeStr = data.usedVolumeStr.replace(/,\d+/, '')
-    usedVolumeNum = data.usedVolume / 1024 / 1024 / 1024
+    usedVolumeStr = data.usedVolumeStr//.replace(/,\d+/, '')
+    //usedVolumeNum = data.usedVolume / 1024 / 1024 / 1024
+    initialVolumeStr = data.initialVolumeStr
     initialVolumeNum = Math.abs(data.initialVolume.toFixed() / 1024 / 1024 / 1024)
-    usedVolume = String(Math.round((usedVolumeNum + Number.EPSILON) * 100) / 100).replace(',', '.')
+    usedVolume = String(Math.round(((data.usedVolume/1024/1024/1024) + Number.EPSILON) * 100) / 100).replace(',', '.')
     initialVolume = String(Math.round((initialVolumeNum + Number.EPSILON) * 100) / 100).replace(',', '.')
     initialVolume = parseFloat(initialVolume).toFixed(2)
-    remainingVolumeNum = (data.initialVolume - data.usedVolume) / 1024 / 1024 / 1024
-    remainingVolume = String(Math.round((remainingVolumeNum + Number.EPSILON) * 100) / 100).replace(',', '.')
+    //remainingVolumeNum = (data.initialVolume - data.usedVolume) / 1024 / 1024 / 1024
+    //remainingVolume = String(Math.round(((data.initialVolume - data.usedVolume) / 1024 / 1024 / 1024 + Number.EPSILON) * 100) / 100).replace(',', '.')
     remainingVolumeMB = Math.abs((data.initialVolume - data.usedVolume) / 1024 / 1024).toFixed(2)
-    remainungVolumeGB = Math.abs((remainingVolumeMB) / 1024).toFixed(2)
+    remainungVolumeGB = Math.abs((data.initialVolume - data.usedVolume) / 1024 / 1024 / 1024).toFixed(2)
+    
     lastUpdate = df.string(new Date(data.usedAt))
     nextUpdate = df.string(new Date (10800*1000+data.usedAt))
-    datas = {unlimited, usedPercentage, remainingTimeStr, passName, usedVolumeStr, usedVolumeNum, initialVolumeNum, usedVolume, initialVolume, remainingVolumeNum, remainingVolume, remainingVolumeMB, remainungVolumeGB, lastUpdate, nextUpdate}
+    
+    var datas = {
+    	unlimited, passName, usedPercentage, remainingTimeStr, usedVolumeStr, initialVolumeStr, initialVolumeNum, usedVolume, initialVolume, remainingVolumeMB, remainungVolumeGB, lastUpdate, nextUpdate
+    	}
      }
+     //log({datas})
   return {fresh, sfSymbol, fontColor, datas}
 };
 
@@ -87,47 +104,77 @@ module.exports.getSF = (ground, name, fontSize, color, sizeW, sizeH) => {
 };
 
 
-module.exports.createCircle = async (value) => {
+module.exports.createCircle = async  (value, initialVolume) => {
   if (value > 1) value /= 100
   if (value < 0) value = 0
   if (value > 1) value = 1
+  
+  	 day = new Date().getDate()//day of the current month
+     dailyUse = (initialVolume / new Date(new Date().getFullYear(), new Date().getDay(), 0).getDate()).toFixed(4)
+     todayStatus = dailyUse * day
+     //pathWidth = (width * todayStatus) / initialVolume > width ? width : (width * todayStatus) / initialVolume
 
   let webView = new WebView()
   await webView.loadHTML('<canvas id="c"></canvas>')
 
   let base64 = await webView.evaluateJavaScript(
     `
-    //let colour = "#FFFFFF",
-    //background = "#000000",
-    size = 57*3,
-    lineWidth = 5*3,
-    percent = ${value * 100}
-      
-    let canvas = document.getElementById('c'),
-    c = canvas.getContext('2d')
-    canvas.width = size
-    canvas.height = size 
-    let posX = canvas.width / 2,
-    posY = canvas.height / 2,
-    onePercent = 360 / 100,
-    result = onePercent * ${value * 100}
-    c.lineCap = 'round'
-    c.beginPath()
-    c.arc( posX, posY, (size-lineWidth-1)/2, (Math.PI/180) * 270, (Math.PI/180) * (270 + 360) )
-    c.strokeStyle = "#000000"
-    c.lineWidth = lineWidth 
-    c.stroke()
-    c.beginPath()
-    c.strokeStyle = "#FFFFFF"
-    c.lineWidth = lineWidth
-    c.arc( posX, posY, (size-lineWidth-1)/2, (Math.PI/180) * 270, (Math.PI/180) * (270 + result) )
-    c.stroke()
+     //set variables
+     let colour = "#FFFFFF",
+     background = "#000000",
+     size = 57 * 3,
+     lineWidth = 5 * 3,
+     percent = ${ value * 100 },
+     indicatorValue = ${ todayStatus };
+     
+     //create canvas ground
+     let canvas = document.getElementById('c'),
+     c = canvas.getContext('2d');
+	 canvas.width = size;
+	 canvas.height = size;
+     posX = canvas.width / 2,
+	 posY = canvas.height / 2,
+     onePercent = 360 / 100,
+     result = onePercent * ${ value * 100 };
+	 c.lineCap = 'round';
+	 
+	 //draw background circle
+	 c.beginPath();
+	 c.arc( posX, posY, ( size - lineWidth - 1 ) / 2, ( Math.PI / 180 ) * 270, ( Math.PI / 180 ) * ( 270 + 360 ) );
+	 c.strokeStyle = background;
+	 c.lineWidth = lineWidth;
+	 c.stroke();
+	 
+	 //draw the progress bar
+	 c.beginPath();
+	 c.strokeStyle = colour;
+	 c.lineWidth = lineWidth;
+	 c.arc( posX, posY, ( size - lineWidth - 1 ) / 2, ( Math.PI / 180 ) * 270, ( Math.PI / 180 ) * ( 270 + result ) );
+	 c.stroke();
+	 
+	 //calc position of status indicator
+	 indicatorAngle = ( 270 + 360 * indicatorValue ) * ( Math.PI / 100 );
+	 indicatorLength = ( size - lineWidth - 1 ) / 2;
+	 lineOffset = 5; //length of progress indicator
+	 
+	 //draw status indicator
+	 c.beginPath();
+	 c.strokeStyle = 'green';
+	 c.lineWidth = 7;
+	 c.moveTo(
+	 	posX + Math.cos( indicatorAngle ) * ( indicatorLength - lineOffset ),
+	 	posY + Math.sin( indicatorAngle ) * ( indicatorLength - lineOffset )
+	 );
+	 c.lineTo(
+     	posX + Math.cos( indicatorAngle ) * ( indicatorLength + lineOffset ),
+     	posY + Math.sin( indicatorAngle ) * ( indicatorLength + lineOffset )
+	 );
+	 c.stroke();
+	 
     completion(canvas.toDataURL().replace("data:image/png;base64,",""))`,
     true
   );
   
- //image = Image.fromData(Data.fromBase64String(base64))
-
   return Image.fromData(Data.fromBase64String(base64))
 };
 
@@ -142,21 +189,15 @@ module.exports.getImageFor = async (fm, dir, name) => {
 
 //Create progress bar
 module.exports.createProgress = (initialVolume, bgColor, fillColor, width, height, value, c1, c2) => {
-  let day = new Date().getDate()//day of the current month
-  let currentDays = new Date(new Date().getFullYear(), new Date().getDay(), 0).getDate()//days of the month
-  let eachStep = Math.round(width/currentDays)//each day as step in the progress bar
-  let calc = day * currentDays
+  	 day = new Date().getDate()//day of the current month
+     dailyUse = (initialVolume / new Date(new Date().getFullYear(), new Date().getDay(), 0).getDate()).toFixed(4)
+     todayStatus = dailyUse * day
   
-  let dailyUse = (initialVolume / currentDays).toFixed(4)
-       console.log({dailyUse})
-  let todayStatus = dailyUse * day
-      //console.log({todayStatus})
-  
-  let context = new DrawContext();
+  let context = new DrawContext()
       context.size = new Size(width, height)  
       context.opaque = false
       context.respectScreenScale = true
-      context.setFillColor(bgColor)//#8E8E92 grau
+      context.setFillColor(bgColor)
   
   let path = new Path()
       path.addRoundedRect(new Rect(0, 0, width, height), c1, c2)
@@ -218,7 +259,7 @@ module.exports.updateCheck = async (fm, modulePath, version) => {
         	throw new Error("Update Complete!")
       }
     }
-  } else  log("\n>> SCRIPT IS UP TO DATE!")
+  } else log("\n>> SCRIPT IS UP TO DATE!")
   
   return {uC, needUpdate}
 };
